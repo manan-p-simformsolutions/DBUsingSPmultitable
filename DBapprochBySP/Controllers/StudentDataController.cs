178ef776d6ac67f43511987d97569e00af5e24f1
Manan <manansoni74@gmail.com>
2021-03-23 10:48:40 +0530

    initial commit

---
+++ DBapprochBySP/Controllers/StudentDataController.cs
@@ -0,0 +1,53 @@
+﻿using DBapprochBySP.Models;
+using LanguageExt.ClassInstances;
+using System;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace DBapprochBySP.Controllers
+{
+    public class StudentDataController : Controller
+    {
+        private readonly StudentEntities1 obj = new StudentEntities1();
+        // GET: StudentData
+        public ActionResult StudentList(int Cpage = 1)
+        {
+
+            ViewBag.PageCount = (int)Math.Ceiling((double)((decimal)obj.StudentDatas.Count() / 2));
+            ViewBag.CPage = Cpage;
+            var s = obj.Database.SqlQuery<StudentList>(
+                "exec Pagination @PageNumber,@PageSize",
+                new SqlParameter("@PageNumber", Cpage), new SqlParameter("@PageSize", 2)).ToList();
+
+
+            /*IList<StudentList_Result> studentList = obj.StudentList().Select(x => new StudentList_Result()
+            {
+                Id = x.Id,
+                Name = x.Name,
+                RollNo = x.RollNo,
+                Class = x.Class,
+                City = x.City,
+                Address = x.Address,
+                Amount = x.Amount,
+                Year = x.Year
+            }).ToList();*/
+             ViewBag.studentList = obj.StudentList().ToList();
+            return View(s);
+        }
+        public ActionResult Edit(int id)
+        {
+            var st = obj.StudentList().FirstOrDefault(x=>x.Id==id);
+            return View(st);
+
+        }
+        [HttpPost]
+        public ActionResult Edit(StudentList_Result st)
+        {
+            obj.UpdateStudent(st.Id, st.Id,st.Name, st.RollNo, Int32.Parse(st.Class), st.City, st.Address, st.Amount, st.Year);
+            return View();
+        }
+
+
+    }
+}
---
+++ DBapprochBySP/Models/StudentList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DBapprochBySP.Models
+{
+    public class StudentList
+    {
+        public int StudentId { get; set; }
+        public Nullable<int> ScholarshipId { get; set; }
+        public string Name { get; set; }
+        public int RollNo { get; set; }
+        public string Class { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public Nullable<decimal> Amount { get; set; }
+        public Nullable<int> Year { get; set; }
+
+    }
+}
